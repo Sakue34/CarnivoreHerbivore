91b59cc5f96ebdf816a444266f57dcce3d0ff39a
Sakue34 <szymox815@gmail.com>
2020-04-27 16:00:43 +0200

    Add missing Plant.java

---
+++ src/main/java/pl/edu/pwr/carnivoreherbivore/Plant.java
@@ -0,0 +1,16 @@
+package pl.edu.pwr.carnivoreherbivore;
+
+class Plant extends Entity {
+    public Plant(float x, float y) {
+        super(x, y);
+    }
+
+    @Override
+    public void updatePosition(float elapsedTime) { }
+
+    @Override
+    public void collideWithEntity(Entity entity) { }
+
+    @Override
+    public void updateAI() { }
+}
