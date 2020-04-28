6bdedea8123fd4f379e927c03ed08da9453b60bb
Sakue34 <szymox815@gmail.com>
2020-04-28 17:31:26 +0200

    Entity initialisation

--- src/main/java/pl/edu/pwr/carnivoreherbivore/EntityManager.java
+++ src/main/java/pl/edu/pwr/carnivoreherbivore/EntityManager.java
@@ -1,7 +1,6 @@
 package pl.edu.pwr.carnivoreherbivore;
 
 import java.util.ArrayList;
-import java.lang.Math;
 
 public class EntityManager {
     private ArrayList<Entity> entityList;
@@ -10,8 +9,23 @@ public class EntityManager {
         entityList.add(entity);
     }
 
-    public EntityManager(int initialNumberOfEntities) {
-        entityList = new ArrayList<>(initialNumberOfEntities);
+    public EntityManager(int numberOfPlants, int numberOfHerbivores, int numberOfCarnivores) {
+        entityList = new ArrayList<>(numberOfPlants + numberOfHerbivores + numberOfCarnivores);
+
+        for(int i = 0; i < numberOfPlants; i++) {
+            entityList.add(new Plant((float)(Math.random() * App.MAP_WIDTH),
+                    (float)(Math.random() * App.MAP_HEIGHT)));
+        }
+
+        for(int i = 0; i < numberOfHerbivores; i++) {
+            entityList.add(new Herbivore((float)(Math.random() * App.MAP_WIDTH),
+                    (float)(Math.random() * App.MAP_HEIGHT)));
+        }
+
+        for(int i = 0; i < numberOfCarnivores; i++) {
+            entityList.add(new Carnivore((float)(Math.random() * App.MAP_WIDTH),
+                    (float)(Math.random() * App.MAP_HEIGHT)));
+        }
     }
 
     public void updateEveryEntity(float elapsedTime) {
@@ -55,7 +69,7 @@ public class EntityManager {
                         }
                     }
                 }
-                entity.updateAI(nearestHerbivore);
+                entity.updateAI(nearestHerbivore, elapsedTime);
             }
             else if (entity instanceof Herbivore) {
                 Carnivore nearestCarnivore = null;
@@ -97,9 +111,9 @@ public class EntityManager {
                     }
                 }
                 if (nearestCarnivore != null) {
-                    entity.updateAI(nearestCarnivore);
+                    entity.updateAI(nearestCarnivore, elapsedTime);
                 } else {
-                    entity.updateAI(nearestPlant);
+                    entity.updateAI(nearestPlant, elapsedTime);
                 }
             }
         }
