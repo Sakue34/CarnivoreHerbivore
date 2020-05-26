edd9e0c72eb7fef2b69fa1180da41fef01dc7797
Sakue34 <szymox815@gmail.com>
2020-05-26 20:05:50 +0200

    Cleanup

--- src/main/java/pl/edu/pwr/carnivoreherbivore/App.java
+++ src/main/java/pl/edu/pwr/carnivoreherbivore/App.java
@@ -1,13 +1,11 @@
 package pl.edu.pwr.carnivoreherbivore;
 
-import pl.edu.pwr.carnivoreherbivore.config.ConfigProvider;
-import pl.edu.pwr.carnivoreherbivore.config.DefaultParametersProvider;
+import pl.edu.pwr.carnivoreherbivore.config.*;
 import pl.edu.pwr.carnivoreherbivore.simulation.Simulation;
 import pl.edu.pwr.carnivoreherbivore.simulation.SimulationCreator;
 import pl.edu.pwr.carnivoreherbivore.simulation.SimulationParameters;
 
 public final class App {
-
     public static void main(String[] args) {
         ConfigProvider configProvider = new DefaultParametersProvider();
         SimulationParameters simulationParameters = configProvider.get();
